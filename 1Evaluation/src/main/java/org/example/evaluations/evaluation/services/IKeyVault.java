be3c22c6dbab6029bbb885f6e44b2d8fe08f1c98
vikash <vikashnitwkumar@gmail.com>
2025-02-14 00:04:31 +0530

    clearing project

--- 1Evaluation/src/main/java/org/example/evaluations/evaluation/services/IKeyVault.java
+++
@@ -1,7 +0,0 @@
-package org.example.evaluations.evaluation.services;
-
-public interface IKeyVault {
-    void saveSecret(String secretName,String secretValue);
-
-    String retrieveSecret(String secretName);
-}
